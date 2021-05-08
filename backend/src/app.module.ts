aa8a7bb89d7970b87865734c4cdd7c4c1f8ad816
Max Atslega <max.atslega@hotmail.com>
2021-05-08 21:12:56 +0200

    remove rate limit

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,18 +1,17 @@
 import { Module } from '@nestjs/common';
-import {DatabaseModule} from "./database/database.module";
-import {JwtConfigService} from "./util/JwtConfigService";
-import {ConfigModule} from "@nestjs/config";
-import {JwtModule} from "@nestjs/jwt";
-import { PassportModule } from "@nestjs/passport";
-import {audioProvider} from "./provider/audio.provider";
-import {AuthService} from "./auth/auth.service";
-import {AuthController} from "./auth/auth.controller";
-import {AudioService} from "./audio/audio.service";
-import {AudioController} from "./audio/audio.controller";
-import {JwtStrategy} from "./auth/ jwt.strategy";
-import {MulterModule} from "@nestjs/platform-express";
-import {RateLimiterInterceptor, RateLimiterModule} from 'nestjs-rate-limiter';
-import {APP_INTERCEPTOR} from "@nestjs/core";
+import { DatabaseModule } from './database/database.module';
+import { JwtConfigService } from './util/JwtConfigService';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { audioProvider } from './provider/audio.provider';
+import { AuthService } from './auth/auth.service';
+import { AuthController } from './auth/auth.controller';
+import { AudioService } from './audio/audio.service';
+import { AudioController } from './audio/audio.controller';
+import { JwtStrategy } from './auth/ jwt.strategy';
+import { MulterModule } from '@nestjs/platform-express';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -26,19 +25,8 @@ import {APP_INTERCEPTOR} from "@nestjs/core";
     JwtModule.registerAsync({ useClass: JwtConfigService }),
     PassportModule,
     DatabaseModule,
-    RateLimiterModule
   ],
   controllers: [AuthController, AudioController],
-  providers: [
-    ...audioProvider,
-    AuthService,
-    AudioService,
-    JwtStrategy,
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: RateLimiterInterceptor,
-    },
-
-  ],
+  providers: [...audioProvider, AuthService, AudioService, JwtStrategy],
 })
 export class AppModule {}
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,29 +1,27 @@
-import { Injectable, UnauthorizedException } from "@nestjs/common";
-import { JwtService } from "@nestjs/jwt";
-import { compareSync, hash } from "bcrypt";
-import { KeyDto } from "../model/key.dto";
-import {BearerTokenDto} from "../model/bearer-token.dto";
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { compareSync } from 'bcrypt';
+import { KeyDto } from '../model/key.dto';
+import { BearerTokenDto } from '../model/bearer-token.dto';
 
 @Injectable()
 export class AuthService {
-    constructor(
-        private jwtService: JwtService,
-    ) {}
+  constructor(private jwtService: JwtService) {}
 
-    async login(userDto: KeyDto): Promise<BearerTokenDto> {
-        if (compareSync(userDto.key, process.env.USER_KEY)) {
-            return {
-                access_token: this.jwtService.sign({
-                    role: "User",
-                }),
-            };
-        }else if(compareSync(userDto.key, process.env.ADMIN_KEY)){
-            return {
-                access_token: this.jwtService.sign({
-                    role: "Admin",
-                }),
-            };
-        }
-        throw new UnauthorizedException();
+  async login(userDto: KeyDto): Promise<BearerTokenDto> {
+    if (compareSync(userDto.key, process.env.USER_KEY)) {
+      return {
+        access_token: this.jwtService.sign({
+          role: 'User',
+        }),
+      };
+    } else if (compareSync(userDto.key, process.env.ADMIN_KEY)) {
+      return {
+        access_token: this.jwtService.sign({
+          role: 'Admin',
+        }),
+      };
     }
+    throw new UnauthorizedException();
+  }
 }
